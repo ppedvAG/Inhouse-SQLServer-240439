57aaf811fda96ebc86259c0787fb1e981b3abdd8
Rauchendraser <andreasr@ppedv.de>
2024-01-17 11:01:54 +0100

    Heap DB Optimierung Messen

---
+++ SQL  Tuning/SQL  Tuning/03_Messen von Abfragen.sql
@@ -0,0 +1,17 @@
+set statistics io, time on
+
+--gilt nur pro aktiver Sitzung
+--ausschalten ! sonst Performanceverlust wg Messen
+
+--IO = Anzahl der Seiten = logosche Lesevorgänge
+--time = CPU Zeit(aufwand) in ms, Dauer der Abfrage in ms
+
+--Ziel : Reduktion
+
+--Seitenauslastung.. veraltet
+dbcc showcontig('kundeumsatz')
+
+--besser
+select * from sys.dm_db_index_physical_stats
+		(db_id(),object_id('Kundeumsatz'),NULL, NULL,'detailed')
+
